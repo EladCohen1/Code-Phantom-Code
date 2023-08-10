80593e0a1797428f9ebbb9d87ea0ca1e11a3b1de
Elad Cohen <eladco13@gmail.com>
2023-08-10 23:48:06 +0300

    Adding cinemachine camera and jump logic

--- Assets/Scripts/Characters/PlayerScript.cs
+++ Assets/Scripts/Characters/PlayerScript.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float speed = 3;
-    public float rotationSpeed = 360;
+    public float rotationSpeed = 720;
+    public float jumpSpeed = 3;
+    public float jumpGracePeriod = 0.1f;
+
 
     private CharacterController characterController;
+    private float ySpeed = 0;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
+    private Animator characterAnimator;
     // Start is called before the first frame update
     void Start()
     {
+        characterAnimator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+    }
+    void HandleMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -24,12 +38,49 @@ public class PlayerScript : MonoBehaviour
         float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
         movementDirection.Normalize();
 
-        characterController.SimpleMove(movementDirection * magnitude);
+        ySpeed += Physics.gravity.y * Time.deltaTime;
 
+        if (characterController.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpButtonPressedTime = Time.time;
+        }
+
+
+        if (Time.time - lastGroundedTime <= jumpGracePeriod)
+        {
+            ySpeed = -0.5f;
+            characterController.stepOffset = originalStepOffset;
+            if (Time.time - jumpButtonPressedTime <= jumpGracePeriod)
+            {
+                ySpeed = jumpSpeed;
+                jumpButtonPressedTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        // rotating the character to face where he is moving
         if (movementDirection != Vector3.zero)
         {
+            characterAnimator.SetBool("IsMoving", true);
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            characterAnimator.SetBool("IsMoving", false);
         }
     }
 }
